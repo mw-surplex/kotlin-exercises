9d1bd23bb8a4a26441f728195b7a6c21e8bdd070
martin.wiesner <martin.wiesner@surplex.com>
2024-10-21 09:57:18 +0200

    Add test for DayOfWeek exercise

--- src/main/kotlin/essentials/enums/DayOfWeek.kt
+++ src/main/kotlin/essentials/enums/DayOfWeek.kt
@@ -1,25 +1,12 @@
 package essentials.enums
 
+// https://kt.academy/article/e_days_of_the_week_enum
+
 fun main() {
-    val friday: DayOfWeek = DayOfWeek.FRIDAY
-    println(friday.dayName) // Friday
-    println(friday.isWeekend) // false
-    val saturday: DayOfWeek = friday.nextDay()
-    println(saturday.dayName) // Saturday
-    println(saturday.isWeekend) // true
-}
-
-enum class DayOfWeek(val isWeekend: Boolean, val dayName: String) {
-    MONDAY(false, "monday"),
-    TUESDAY(false, "tuesday"),
-    WEDNESDAY(false, "wednesday"),
-    THURSDAY(false, "thursday"),
-    FRIDAY(false, "friday"),
-    SATURDAY(true, "saturday"),
-    SUNDAY(true, "sunday");
-
-    fun nextDay(): DayOfWeek =
-         DayOfWeek.entries.let {
-            it[(it.indexOf(this) + 1) % it.size]
-        }
+//    val friday: DayOfWeek = DayOfWeek.FRIDAY
+//    println(friday.dayName) // Friday
+//    println(friday.isWeekend) // false
+//    val saturday: DayOfWeek = friday.nextDay()
+//    println(saturday.dayName) // Saturday
+//    println(saturday.isWeekend) // true
 }
--- src/test/kotlin/essentials/enums/DayOfWeekTest.kt
+++ src/test/kotlin/essentials/enums/DayOfWeekTest.kt
@@ -18,4 +18,39 @@ class DayOfWeekTest {
         assertEquals("tuesday", day.dayName)
         assertEquals(DayOfWeek.WEDNESDAY, day.nextDay())
     }
+
+    @Test
+    fun testWednesday() {
+        val day = DayOfWeek.WEDNESDAY
+        assertEquals("wednesday", day.dayName)
+        assertEquals(DayOfWeek.THURSDAY, day.nextDay())
+    }
+
+    @Test
+    fun testThursday() {
+        val day = DayOfWeek.THURSDAY
+        assertEquals("thursday", day.dayName)
+        assertEquals(DayOfWeek.FRIDAY, day.nextDay())
+    }
+
+    @Test
+    fun testFriday() {
+        val day = DayOfWeek.FRIDAY
+        assertEquals("friday", day.dayName)
+        assertEquals(DayOfWeek.SATURDAY, day.nextDay())
+    }
+
+    @Test
+    fun testSaturday() {
+        val day = DayOfWeek.SATURDAY
+        assertEquals("saturday", day.dayName)
+        assertEquals(DayOfWeek.SUNDAY, day.nextDay())
+    }
+
+    @Test
+    fun testSunday() {
+        val day = DayOfWeek.SUNDAY
+        assertEquals("sunday", day.dayName)
+        assertEquals(DayOfWeek.MONDAY, day.nextDay())
+    }
 }
